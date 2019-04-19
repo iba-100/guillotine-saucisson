6050d6492fcf377889c4fb5d440971c5081c290b
tiboCorb <corbrionthibault@gmail.com>
2019-04-19 15:38:27 +0200

    lotery page

--- src/Controller/GiftController.php
+++ src/Controller/GiftController.php
@@ -52,9 +52,13 @@ public function lotery(){
     $statement = $connection->prepare("SELECT * from gift order by RAND() limit 1");
     $statement->execute();
     $results = $statement->fetchAll();
-    echo "<h1> LE GAGNANT EST : </h1>". $results[0]['email'];
-    echo "<h1> PRENOM  : </h1>". $results[0]['firstname'];
-    echo "<h1> NOM  </h1>". $results[0]['lastname'];
+    echo '<div style="background: lightgray; position : absolute; top : 0; bottom: 0; right:0; left:0;">';
+    echo "<div  style= \"border: 2px solid white; border-radius :20px ; width : 500px; margin: auto; padding: 20px; background: white\">";
+    echo "<h1 style=\"font-weight: bold; color : grey\"> LE GAGNANT EST : </h1>". $results[0]['email'];
+    echo "<h1 style=\"font-weight: bold; color : grey\"> PRENOM  : </h1>". $results[0]['firstname'];
+    echo "<h1 style=\"font-weight: bold; color : grey\"> NOM  </h1>". $results[0]['lastname'];
+    echo"</div>";
+    echo"</div>";
   
     exit();
 }
